db0bc426f97f3a9fdba0b7291e6b12fee98d6b6f
Mul Yadi <blscyber01@gmail.com>
2017-04-02 03:49:23 +0700

    Create ga_ads.js

---
+++ ga_ads.js
@@ -0,0 +1,139 @@
+var abgp = {
+	elp: document.getElementById('abgcp'),
+	el: document.getElementById('abgc'),
+	ael: document.getElementById('abgs'),
+	iel: document.getElementById('abgb'),
+	hw: 15,
+	sw: 96,
+	hh: 15,
+	sh: 15,
+	himg: 'https://tpc.googlesyndication.com' + '/pagead/images/abg/icon.png',
+	simg: 'https://tpc.googlesyndication.com/pagead/images/abg/en.png',
+	alt: 'Ads by Google',
+	t: 'Ads by',
+	tw: 34,
+	t2: 'Google',
+	t2w: 38,
+	tbo: 0,
+	att: 'adsbygoogle',
+	ff: '',
+	halign: 'right',
+	fe: false,
+	iba: false,
+	lttp: true,
+	uic: true,
+	uit: true,
+	ict: document.getElementById('cbb'),
+	ci: '',
+	icd: {
+		"creatives": [],
+		"height": 600,
+		"width": 300,
+		"attribution": {
+			"user_feedback_data": {
+				"mute_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"pub_feedback_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"conversion_url": "https://googleads.g.doubleclick.net/pagead/conversion/?ai=CcqzBVwjgWLjwMYnSogOXtI6wCP_X3K9IgN2M0JgFh8WzmJ4FEAEgrc6NJGDpquKD5A2gAfmSlbkDyAECqAMByAPJBKoEhwFP0JjsbHGU0rkXcLPBZaTTHNSHiz8CsAoCrO8xkm_mcCfJrhqkId0cQqVtKFkToN3NkWdXilaYNyDTRyLwVAVP0QXuFKF37Vy9LScn0rFUd1LokPYiUhwepZ2aYWFYC2VJ-kZbpH1ySab-Ut5PgxYEhSb5Gi6jWhQRUajXHHGRkQSh7Iy9zXGgBgKAB-_s6kaoB6a-G9gHAdIIBQiAARAB2BMM\u0026sigh=s-NOMaXm5qM",
+				"close_button_token": "d6FcC7fL_nsIgN2M0JgFEIaBzN8CGPfR-EciE2VsZW1lbnRzLmVudmF0by5jb20yCAgFExiBmhcUQhdjYS1wdWItMzg3MDExMzgwMjg1MDA1M0gFWAJwAQ",
+				"interaction_conversion": {
+					"label": "user_feedback_menu_interaction",
+					"label_instance": "",
+					"include_close_button_token": false
+				},
+				"survey_header": "What was wrong with this ad?",
+				"back_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/back_blue.png",
+				"mute_confirmation_header": "Thanks for the feedback!",
+				"mute_confirmation_text": "We’ll review this ad to improve the experience in the future.",
+				"pub_feedback_confirmation_header": "Thanks for the feedback!",
+				"pub_feedback_confirmation_text": "We’ll use your feedback to review ads on this site.",
+				"encoded_cookie": "CAASEuRokx9XR0qc3_f-dMjDPL-tOA",
+				"undo_text": "Undo",
+				"close_message": "{\"msg_type\":\"ablate-me\",\"key_value\":[{\"key\":\"collapse-after-close\",\"value\":\"animate\"},{\"key\":\"secs-to-countdown\",\"value\":\"1\"},{\"key\":\"countdown\",\"value\":\"3\"},{\"key\":\"message-tick\",\"value\":\"1\"}],\"googMsgType\":\"sth\"}",
+				"closing_countdown_text": "Closing ad: %1$d",
+				"attribution_text": "Ads by Google",
+				"attribution_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/abg_blue.png",
+				"attribution_destination_url": "https://www.google.com/url?ct=abg\u0026q=https://www.google.com/adsense/support/bin/request.py%3Fcontact%3Dabg_afc%26url%3Dhttps://www.jojothemes.com/%26gl%3DID%26hl%3Den%26client%3Dca-pub-3870113802850053%26ai0%3DCcqzBVwjgWLjwMYnSogOXtI6wCP_X3K9IgN2M0JgFh8WzmJ4FEAEgrc6NJGDpquKD5A2gAfmSlbkDyAECqAMByAPJBKoEhwFP0JjsbHGU0rkXcLPBZaTTHNSHiz8CsAoCrO8xkm_mcCfJrhqkId0cQqVtKFkToN3NkWdXilaYNyDTRyLwVAVP0QXuFKF37Vy9LScn0rFUd1LokPYiUhwepZ2aYWFYC2VJ-kZbpH1ySab-Ut5PgxYEhSb5Gi6jWhQRUajXHHGRkQSh7Iy9zXGgBgKAB-_s6kaoB6a-G9gHAdIIBQiAARAB2BMM\u0026usg=AFQjCNEMgSl4Q-eWQ7OCBa1jqqah-4e4Sw",
+				"ad_feedback_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"is_rtl_language": false,
+				"feedback_options": [{
+					"text": "Report this ad",
+					"conversion": {
+						"label": "user_feedback_menu_option",
+						"label_instance": "1",
+						"include_close_button_token": true
+					},
+					"survey": {
+						"header": "What was wrong with this ad?",
+						"options": [{
+							"text": "Seen this ad multiple times",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "2",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Not interested in this ad",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "12",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Ad covered content",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "3",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Already bought this",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "11",
+								"include_close_button_token": true
+							}
+						}]
+					},
+					"undo_conversion": {
+						"label": "user_feedback_undo",
+						"label_instance": "1",
+						"include_close_button_token": true
+					}
+				}],
+				"mute_panel_data": {
+					"adchoices_icon_url": "https://googleads.g.doubleclick.net/pagead/images/adchoices/iconx2-000000.png",
+					"adchoices_button_text": "AdChoices",
+					"closed_message_text": "Ad closed by %1$s",
+					"enable_lightbox": false,
+					"google_logo_url": "https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_dark_color_84x28dp.png",
+					"report_ad_button_text": "Report this ad",
+					"confirmation_text": "We'll try not to show that ad again",
+					"settings_button_first": false,
+					"ads_by_google_icon_url": "https://googleads.g.doubleclick.net/pagead/images/abg/iconx2-000000.png",
+					"ads_by_google_button_text": "Ads by Google",
+					"protocol_gstatic_host": "https://www.gstatic.com",
+					"jake_mta_context": ""
+				},
+				"why_this_ad_token": "AB3afGEAAAIdeyJpbWFnZV93aHlfdGhpc19hZCI6eyJsYW5kaW5nX3VybCI6Imh0dHBzOi8vZWxlbWVudHMuZW52YXRvLmNvbS9scC9kZXNpZ24vIiwiaW1hZ2VfdXJsIjoiaHR0cHM6Ly90cGMuZ29vZ2xlc3luZGljYXRpb24uY29tL3NpbWdhZC8xNzQ5MTE0ODEzMzcwNjgyODk2MiIsImltYWdlX3dpZHRoIjozMDAsImltYWdlX2hlaWdodCI6NjAwfSwidGFyZ2V0aW5nX3JlYXNvbnMiOnsiYWRfcmVhc29uIjpbMSw5XSwiZXhwbGFuYXRpb24iOnsiaW50cm9kdWN0aW9uIjoiVGhpcyBpcyBhIGxpc3Qgb2YgdGhlIGluZm9ybWF0aW9uIHNvdXJjZXMgdXNlZCB0byBkZXRlcm1pbmUgdGhhdCB0aGlzIGFkIGJlIHNob3duIHRvIHlvdToiLCJpdGVtIjpbeyJkZXNjcmlwdGlvbiI6IllvdXIgdmlzaXQgdG8gdGhlIGFkdmVydGlzZXIncyB3ZWJzaXRlLiJ9LHsiZGVzY3JpcHRpb24iOiJUaGUgdGltZSBvZiBkYXksIHRoZSB3ZWJzaXRlIHlvdSB3ZXJlIHZpZXdpbmcgb3IgeW91ciBnZW5lcmFsIGxvY2F0aW9uIChmb3IgZXhhbXBsZSBjb3VudHJ5IG9yIGNpdHkpLiJ9XX19fYVexzT0C1b0qDcZke25OHUx6N7YjvZC2X23SdW3Hj317AaBwl9ZIjl7H316eqtaOVP8mgAFyg76r1ITETtr8T-GS5rVkFdekIZQ37bqhVuIrS1QnrQhNnkOH8noPFHbGw5QVlvpUpsWNAD1BHpBPhZLxsJbBjPBmtCnW10cGljSh1bwg5egfOe_gWTeP6n3jRYfnr6wym52Ljc5-tWmGnhbnU9YgcU7kfE9JGOw8TGejamQL-1UsN2ZVaGqtrPHIuNxtTiicKoLn_KpsKvNYwjV-IYcJyYINs0N7eKolwxj1Zen6qZw9h6KUlMMnlqBuDgWa6n3ezb1ao06pnKPpSI,tHawHS55xM3T7z4WzdqHQg",
+				"why_this_ad_url": "https://www.google.com/ads/preferences/whythisad/en-US/F6rgORqAxhmfFRqM/#/AB3afGEAAAIdeyJpbWFnZV93aHlfdGhpc19hZCI6eyJsYW5kaW5nX3VybCI6Imh0dHBzOi8vZWxlbWVudHMuZW52YXRvLmNvbS9scC9kZXNpZ24vIiwiaW1hZ2VfdXJsIjoiaHR0cHM6Ly90cGMuZ29vZ2xlc3luZGljYXRpb24uY29tL3NpbWdhZC8xNzQ5MTE0ODEzMzcwNjgyODk2MiIsImltYWdlX3dpZHRoIjozMDAsImltYWdlX2hlaWdodCI6NjAwfSwidGFyZ2V0aW5nX3JlYXNvbnMiOnsiYWRfcmVhc29uIjpbMSw5XSwiZXhwbGFuYXRpb24iOnsiaW50cm9kdWN0aW9uIjoiVGhpcyBpcyBhIGxpc3Qgb2YgdGhlIGluZm9ybWF0aW9uIHNvdXJjZXMgdXNlZCB0byBkZXRlcm1pbmUgdGhhdCB0aGlzIGFkIGJlIHNob3duIHRvIHlvdToiLCJpdGVtIjpbeyJkZXNjcmlwdGlvbiI6IllvdXIgdmlzaXQgdG8gdGhlIGFkdmVydGlzZXIncyB3ZWJzaXRlLiJ9LHsiZGVzY3JpcHRpb24iOiJUaGUgdGltZSBvZiBkYXksIHRoZSB3ZWJzaXRlIHlvdSB3ZXJlIHZpZXdpbmcgb3IgeW91ciBnZW5lcmFsIGxvY2F0aW9uIChmb3IgZXhhbXBsZSBjb3VudHJ5IG9yIGNpdHkpLiJ9XX19fYVexzT0C1b0qDcZke25OHUx6N7YjvZC2X23SdW3Hj317AaBwl9ZIjl7H316eqtaOVP8mgAFyg76r1ITETtr8T-GS5rVkFdekIZQ37bqhVuIrS1QnrQhNnkOH8noPFHbGw5QVlvpUpsWNAD1BHpBPhZLxsJbBjPBmtCnW10cGljSh1bwg5egfOe_gWTeP6n3jRYfnr6wym52Ljc5-tWmGnhbnU9YgcU7kfE9JGOw8TGejamQL-1UsN2ZVaGqtrPHIuNxtTiicKoLn_KpsKvNYwjV-IYcJyYINs0N7eKolwxj1Zen6qZw9h6KUlMMnlqBuDgWa6n3ezb1ao06pnKPpSI,tHawHS55xM3T7z4WzdqHQg\u0026d6FcC7fL_nsIgN2M0JgFEIaBzN8CGPfR-EciE2VsZW1lbnRzLmVudmF0by5jb20yCAgFExiBmhcUQhdjYS1wdWItMzg3MDExMzgwMjg1MDA1M0gFWAJwAQ\u0026https://googleads.g.doubleclick.net/pagead/conversion/?ai=CcqzBVwjgWLjwMYnSogOXtI6wCP_X3K9IgN2M0JgFh8WzmJ4FEAEgrc6NJGDpquKD5A2gAfmSlbkDyAECqAMByAPJBKoEhwFP0JjsbHGU0rkXcLPBZaTTHNSHiz8CsAoCrO8xkm_mcCfJrhqkId0cQqVtKFkToN3NkWdXilaYNyDTRyLwVAVP0QXuFKF37Vy9LScn0rFUd1LokPYiUhwepZ2aYWFYC2VJ-kZbpH1ySab-Ut5PgxYEhSb5Gi6jWhQRUajXHHGRkQSh7Iy9zXGgBgKAB-_s6kaoB6a-G9gHAdIIBQiAARAB2BMM\u0026sigh=s-NOMaXm5qM",
+				"why_this_ad_text": "Why this ad?"
+			}
+		},
+		"flags": [{
+			"name": "show_feedback_survey_button",
+			"value": "true"
+		}, {
+			"name": "jake_dimension_selection_strategy",
+			"value": "adslotconfig"
+		}, {
+			"name": "jake_transition_timeout",
+			"value": "-1"
+		}, {
+			"name": "enable_jake_why_this_ad",
+			"value": "true"
+		}]
+	},
+	uaal: true,
+	opi: false,
+	ti: false
+};
