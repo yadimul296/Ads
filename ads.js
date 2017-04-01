5ab411fd6a0efa6e6a550659c90218651ecccdea
Mul Yadi <blscyber01@gmail.com>
2017-04-02 04:26:59 +0700

    Update and rename ga_ads.js to ads.js

---
+++ ads.js
@@ -0,0 +1,138 @@
+var abgp = {
+	elp: document.getElementById('abgcp'),
+	el: document.getElementById('abgc'),
+	ael: document.getElementById('abgs'),
+	iel: document.getElementById('abgb'),
+	hw: 15,
+	sw: 96,
+	hh: 15,
+	sh: 15,
+	himg: 'https://tpc.googlesyndication.com' + '/pagead/images/abg/icon.png',
+	simg: 'https://tpc.googlesyndication.com/pagead/images/abg/en.png',
+	alt: 'Ads by Google',
+	t: 'Ads by',
+	tw: 34,
+	t2: 'Google',
+	t2w: 38,
+	tbo: 0,
+	att: 'adsbygoogle',
+	ff: '',
+	halign: 'right',
+	fe: false,
+	iba: false,
+	lttp: true,
+	uic: true,
+	uit: true,
+	ict: document.getElementById('cbb'),
+	ci: '',
+	icd: {
+		"creatives": [],
+		"height": 600,
+		"width": 300,
+		"attribution": {
+			"user_feedback_data": {
+				"mute_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"pub_feedback_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"conversion_url": "https://googleads.g.doubleclick.net/pagead/conversion/",
+				"close_button_token": "FEVk4qH21KsI1P_o3cEEEM_cqVcY16vUKyISd3d3LnNybXVuaXYuYWMuaW4vMggIBRMY07MhFEIXY2EtcHViLTM4NzAxMTM4MDI4NTAwNTNIAFgCcAR",
+				"interaction_conversion": {
+					"label": "user_feedback_menu_interaction",
+					"label_instance": "",
+					"include_close_button_token": false
+				},
+				"survey_header": "What was wrong with this ad?",
+				"back_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/back_blue.png",
+				"mute_confirmation_header": "Thanks for the feedback!",
+				"mute_confirmation_text": "We’ll review this ad to improve the experience in the future.",
+				"pub_feedback_confirmation_header": "Thanks for the feedback!",
+				"pub_feedback_confirmation_text": "We’ll use your feedback to review ads on this site.",
+				"encoded_cookie": "CAASEuRocX2zgV8zcEUVzCeIrSl8cw",
+				"undo_text": "Undo",
+				"close_message": "{\"msg_type\":\"ablate-me\",\"key_value\":[{\"key\":\"collapse-after-close\",\"value\":\"animate\"},{\"key\":\"secs-to-countdown\",\"value\":\"1\"},{\"key\":\"countdown\",\"value\":\"3\"},{\"key\":\"message-tick\",\"value\":\"1\"}],\"googMsgType\":\"sth\"}",
+				"closing_countdown_text": "Closing ad: %1$d",
+				"attribution_text": "Ads by Google",
+				"attribution_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/abg_blue.png",
+				"attribution_destination_url": "https://www.idnull.us",
+				"ad_feedback_icon_url": "https://googleads.g.doubleclick.net/pagead/images/mtad/x_blue.png",
+				"is_rtl_language": false,
+				"feedback_options": [{
+					"text": "Report this ad",
+					"conversion": {
+						"label": "user_feedback_menu_option",
+						"label_instance": "1",
+						"include_close_button_token": true
+					},
+					"survey": {
+						"header": "What was wrong with this ad?",
+						"options": [{
+							"text": "Ad was inappropriate",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "8",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Not interested in this ad",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "7",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Ad covered content",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "3",
+								"include_close_button_token": true
+							}
+						}, {
+							"text": "Seen this ad multiple times",
+							"conversion": {
+								"label": "mute_survey_option",
+								"label_instance": "2",
+								"include_close_button_token": true
+							}
+						}]
+					},
+					"undo_conversion": {
+						"label": "user_feedback_undo",
+						"label_instance": "1",
+						"include_close_button_token": true
+					}
+				}],
+				"mute_panel_data": {
+					"adchoices_icon_url": "https://googleads.g.doubleclick.net/pagead/images/adchoices/iconx2-000000.png",
+					"adchoices_button_text": "AdChoices",
+					"closed_message_text": "Ad closed by %1$s",
+					"enable_lightbox": false,
+					"google_logo_url": "https://www.gstatic.com/images/branding/googlelogo/2x/googlelogo_dark_color_84x28dp.png",
+					"report_ad_button_text": "Report this ad",
+					"confirmation_text": "We'll try not to show that ad again",
+					"settings_button_first": false,
+					"ads_by_google_icon_url": "https://googleads.g.doubleclick.net/pagead/images/abg/iconx2-000000.png",
+					"ads_by_google_button_text": "Ads by Google",
+					"protocol_gstatic_host": "https://www.gstatic.com",
+					"jake_mta_context": ""
+				},
+				"why_this_ad_url": "https://www.idnull.us",
+				"why_this_ad_text": "Why this ad?"
+			}
+		},
+		"flags": [{
+			"name": "show_feedback_survey_button",
+			"value": "true"
+		}, {
+			"name": "jake_dimension_selection_strategy",
+			"value": "adslotconfig"
+		}, {
+			"name": "jake_transition_timeout",
+			"value": "-1"
+		}, {
+			"name": "enable_jake_why_this_ad",
+			"value": "true"
+		}]
+	},
+	uaal: true,
+	opi: false,
+	ti: false
+};
